80452a629728f78606a54c0ace4a56811ef79977
Fabricio <fabricioanalista@outlook.com.br>
2024-02-28 17:14:58 -0300

    Melhoramento dos models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,6 @@ class Restaurant < ApplicationRecord
   belongs_to :category
   has_many :product_categories
   has_many :orders
+
+  validates :name, :delivery_tax, :city, neigborhood, :street, :number, presence: true
 end
